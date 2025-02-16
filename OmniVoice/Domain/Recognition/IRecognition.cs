68a2aa284ccd2f7b0eea977e7403c29a77473995
reMud <killersan0010@gmail.com>
2025-02-17 01:03:30 +0500

    feat: add IRecognition

---
+++ OmniVoice/Domain/Recognition/IRecognition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniVoice.Domain.Recognition
+{
+    internal interface IRecognition
+    {
+        public bool Accept(byte[] buffer, int length);
+        public string PartialResult();
+        public string Result();
+        public void Reset();
+    }
+}
