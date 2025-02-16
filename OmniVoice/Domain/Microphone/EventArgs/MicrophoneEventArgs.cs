24eaed1936bdb6d6d618b252c4910b41a36cf759
reMud <killersan0010@gmail.com>
2025-02-17 01:05:42 +0500

    feat: add IMicrophone & MicrophoneEventArgs

---
+++ OmniVoice/Domain/Microphone/EventArgs/MicrophoneEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniVoice.Domain.Microphone.EventArgs
+{
+    internal class MicrophoneEventArgs : System.EventArgs
+    {
+        public readonly int Length;
+        public readonly byte[] Buffer;
+
+        public MicrophoneEventArgs(byte[] buffer, int length)
+        {
+            Buffer = buffer;
+            Length = length;
+        }
+    }
+}
---
+++ OmniVoice/Domain/Microphone/IMicrophone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OmniVoice.Domain.Microphone.EventArgs;
+
+namespace OmniVoice.Domain.Microphone
+{
+    internal interface IMicrophone
+    {
+        public event EventHandler<MicrophoneEventArgs> DataAvailable;
+        public void SetInputDevice(int deviceId);
+        public void Start();
+        public void Stop();
+    }
+}
