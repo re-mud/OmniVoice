0861379be368fd327d34057797f4d0aebcccd66c
reMud <killersan0010@gmail.com>
2025-03-15 13:16:43 +0500

    docs(IParses): add xml comments

--- src/Domain/Analyzers/IParser.cs
+++ src/Domain/Analyzers/IParser.cs
@@ -2,6 +2,12 @@
 
 public interface IParser
 {
+    /// <summary>
+    /// the probability of containing data in a row is subtracted
+    /// </summary>
     float Probability(string text);
+    /// <summary>
+    /// basic logic of data extraction
+    /// </summary>
     object ExtractData(string text);
 }
