08d3b2e48bddd8f817e1ac3b78eb765d10b4a8e9
reMud <killersan0010@gmail.com>
2025-03-15 11:41:32 +0500

    feat: add ICommand

---
+++ src/Domain/Command/ICommand.cs
@@ -0,0 +1,23 @@
+﻿namespace OmniVoice.Domain.Command;
+
+public interface ICommand
+{
+    /// <summary>
+    /// list of required parsers
+    /// </summary>
+    string[] RequiredParams { get; }
+    /// <summary>
+    /// probability calculation
+    /// </summary>
+    /// <returns>[0.0f, 1.0f]</returns>
+    float Probability(string text);
+    /// <summary>
+    /// execute command
+    /// </summary>
+    /// <param name="args">data from parsers</param>
+    void Execute(object[] args);
+    /// <summary>
+    /// string display of the command, used for hints
+    /// </summary>
+    string GetCommand();
+}
