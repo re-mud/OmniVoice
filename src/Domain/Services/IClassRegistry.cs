8fa82cfa05681d7c44975fad6fcd3ed79df55e5a
reMud <killersan0010@gmail.com>
2025-03-23 00:39:58 +0500

    feat: add IClassRegistry

---
+++ src/Domain/Services/IClassRegistry.cs
@@ -0,0 +1,10 @@
+﻿namespace OmniVoice.Domain.Services;
+
+public interface IClassRegistry
+{
+    void AddElement<T>(string key, T element) where T : class;
+    T? GetElement<T>(string key) where T : class;
+    void RemoveElement(string key);
+    T[] GetElements<T>() where T : class;
+    string[] GetKeys<T>() where T : class;
+}
