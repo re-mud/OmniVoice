a629bd9e8eca8fe71bc9fe5188d0bac4cc606970
reMud <161937535+re-mud@users.noreply.github.com>
2025-03-26 22:08:10 +0500

    test: add unit tests for Parse from TokenCommand

---
+++ tests/Domain.UnitTest/Command/TokenCommandTests.cs
@@ -0,0 +1,34 @@
+﻿using OmniVoice.Domain.Command;
+
+namespace Domain.UnitTest.Command;
+
+[TestClass]
+public class TokenCommandTests
+{
+    [TestMethod]
+    public void Parse_WhenTextContainsTokens_ShouldReturnCorrectResult()
+    {
+        var tokens = new[] { "a test sentence" };
+        var requiredParams = new string[] { };
+        var command = new TokenCommand(tokens, requiredParams);
+        string inputText = "this is a test sentence";
+
+        var result = command.Parse(inputText);
+
+        Assert.AreEqual("this is ", result.RemainingText);
+        Assert.AreEqual(1, result.Probability);
+    }
+    [TestMethod]
+    public void Parse_WhenTextNotContainsTokens_ShouldReturnCorrectResult()
+    {
+        var tokens = new[] { "dog", "cat" };
+        var requiredParams = new string[] { };
+        var command = new TokenCommand(tokens, requiredParams);
+        string inputText = "this is a test sentence";
+
+        var result = command.Parse(inputText);
+
+        Assert.AreEqual("this is a test sentence", result.RemainingText);
+        Assert.AreEqual(0, result.Probability);
+    }
+}
