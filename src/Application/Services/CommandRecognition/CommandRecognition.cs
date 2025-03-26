5c436a14d1cf25b4730a0311b2667404b69399e2
reMud <161937535+re-mud@users.noreply.github.com>
2025-03-26 21:55:52 +0500

    test: add unit test for Domain

---
+++ src/Application/Services/CommandRecognition/CommandRecognition.cs
@@ -0,0 +1,25 @@
+﻿using OmniVoice.Domain.Analyzers;
+using OmniVoice.Domain.Command.Interfaces;
+
+namespace OnniVoice.Application.Services.CommandRecognition;
+
+public class CommandRecognition
+{
+    private ICommand[] _commands;
+    private IParser[] _parsers;
+
+    public void SetCommands(ICommand[] commands)
+    {
+        _commands = commands;
+    }
+
+    public void SetParcers(IParser[] parsers)
+    {
+        _parsers = parsers;
+    }
+
+    public void Recognize(string text) 
+    {
+
+    }
+}
