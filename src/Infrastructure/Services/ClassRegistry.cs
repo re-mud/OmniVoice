15b076e3b4a584da90c42b005e1ad3ddd7aa6fe5
reMud <161937535+re-mud@users.noreply.github.com>
2025-03-30 20:06:17 +0500

    refactor(ClassRegistry): optimize & add Exceptions

--- src/Infrastructure/Services/ClassRegistry.cs
+++ src/Infrastructure/Services/ClassRegistry.cs
@@ -4,31 +4,56 @@ namespace OmniVoice.Infrastructure.Services;
 
 public class ClassRegistry : IClassRegistry
 {
-    private Dictionary<string, object?> _data = new();
+    private readonly Dictionary<Type, Dictionary<string, object>> _storage = new();
 
     public void AddElement<T>(string key, T element) where T : class
     {
-        _data[key] = element;
+        if (key == null) throw new ArgumentNullException(nameof(key));
+        if (element == null) throw new ArgumentNullException(nameof(element));
+
+        var type = typeof(T);
+        if (!_storage.ContainsKey(type))
+        {
+            _storage[type] = new Dictionary<string, object>();
+        }
+
+        _storage[type][key] = element;
     }
 
     public T? GetElement<T>(string key) where T : class
     {
-        _data.TryGetValue(key, out var value);
-        return value as T;
-    }
+        if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
 
-    public T[] GetElements<T>() where T : class
-    {
-        return _data.Values.OfType<T>().ToArray();
-    }
+        var type = typeof(T);
+        if (!_storage.ContainsKey(type)) return null;
+        if (!_storage[type].ContainsKey(key)) return null;
 
-    public string[] GetKeys<T>() where T : class
-    {
-        return _data.Where(kv => kv.Value is T).Select(kv => kv.Key).ToArray();
+        return _storage[type][key] as T;
     }
 
     public void RemoveElement(string key)
     {
-        _data.Remove(key);
+        if (key == null) throw new ArgumentNullException(nameof(key));
+
+        foreach (var typeDict in _storage.Values)
+        {
+            typeDict.Remove(key);
+        }
+    }
+
+    public T[] GetElements<T>() where T : class
+    {
+        var type = typeof(T);
+        if (!_storage.ContainsKey(type)) return Array.Empty<T>();
+
+        return _storage[type].Values.Cast<T>().ToArray();
+    }
+
+    public string[] GetKeys<T>() where T : class
+    {
+        var type = typeof(T);
+        if (!_storage.ContainsKey(type)) return Array.Empty<string>();
+
+        return _storage[type].Keys.ToArray();
     }
 }
