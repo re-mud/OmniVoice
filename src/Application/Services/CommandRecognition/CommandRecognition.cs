2160d5698417ddcb5c02c050a6330437c77623cb
reMud <161937535+re-mud@users.noreply.github.com>
2025-03-31 19:03:44 +0500

    feat: add service CommandRecognition

--- src/Application/Services/CommandRecognition/CommandRecognition.cs
+++ src/Application/Services/CommandRecognition/CommandRecognition.cs
@@ -1,25 +1,66 @@
-﻿using OmniVoice.Domain.Analyzers;
+﻿using OmniVoice.Application.Services.CommandRecognition;
+using OmniVoice.Domain.Analyzers;
 using OmniVoice.Domain.Command.Interfaces;
+using OmniVoice.Domain.Command.Models;
 
 namespace OnniVoice.Application.Services.CommandRecognition;
 
 public class CommandRecognition
 {
-    private ICommand[] _commands;
-    private IParser[] _parsers;
+    private Dictionary<string, ICommand>? _commands;
+    private Dictionary<string, IParser>? _parsers;
 
-    public void SetCommands(ICommand[] commands)
+    /// <exception cref="ArgumentNullException"></exception>
+    public void SetCommands(Dictionary<string, ICommand> commands)
     {
+        ArgumentNullException.ThrowIfNull(commands);
+
         _commands = commands;
     }
 
-    public void SetParcers(IParser[] parsers)
+    /// <exception cref="ArgumentNullException"></exception>
+    public void SetParsers(Dictionary<string, IParser> parsers)
     {
+        ArgumentNullException.ThrowIfNull(parsers);
+
         _parsers = parsers;
     }
 
-    public void Recognize(string text) 
+    /// <exception cref="ArgumentNullException"></exception>
+    public CommandRecognitionResult[] Recognize(string text)
     {
+        if (_commands == null) throw new ArgumentNullException("Commands is not set");
+        if (_parsers == null) throw new ArgumentNullException("Parcers is not set");
 
+        List<CommandRecognitionResult> results = new List<CommandRecognitionResult>();
+
+        foreach (var kvp in _commands)
+        {
+            ICommand command = kvp.Value;
+            CommandParseResult result = command.Parse(text);
+
+            if (result.Probability < 0.8) continue;
+
+            // params for command
+            List<object> extractedParams = new();
+
+            foreach (var param in command.RequiredParams)
+            {
+                if (!_parsers.TryGetValue(param, out IParser parser)) break;
+
+                object extractData = parser.ExtractData(result.RemainingText);
+
+                if (extractData == null) break;
+
+                extractedParams.Add(param);
+            }
+
+            if (extractedParams.Count != command.RequiredParams.Length) continue;
+
+            results.Add(new CommandRecognitionResult(
+                kvp.Key, command, result.Probability, extractedParams.ToArray()));
+        }
+
+        return results.ToArray();
     }
 }
---
+++ src/Application/Services/CommandRecognition/CommandRecognitionResult.cs
@@ -0,0 +1,16 @@
+﻿using OmniVoice.Domain.Command.Interfaces;
+
+namespace OmniVoice.Application.Services.CommandRecognition;
+
+public class CommandRecognitionResult(string key, ICommand command, float probability, object[] extractedParams)
+{
+    public readonly string Key = key;
+    public readonly ICommand Command = command;
+    public readonly float Probability = probability;
+    public readonly object[] ExtractedParams = extractedParams;
+
+    public void Execute()
+    {
+        Command.Execute(ExtractedParams);
+    }
+} 
