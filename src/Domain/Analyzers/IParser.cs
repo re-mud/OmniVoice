c67ac978968a1a3d1ab276e46c680f6ae8646ef3
reMud <161937535+re-mud@users.noreply.github.com>
2025-03-31 18:55:49 +0500

    feat(IParser): simplification interface
    remove method Probability

--- src/Domain/Analyzers/IParser.cs
+++ src/Domain/Analyzers/IParser.cs
@@ -3,10 +3,6 @@
 public interface IParser
 {
     /// <summary>
-    /// the probability of containing data in a row is subtracted
-    /// </summary>
-    float Probability(string text);
-    /// <summary>
     /// basic logic of data extraction
     /// </summary>
     object ExtractData(string text);
