016c76c6eadeff6d9d860f25029d8fb7f24ae213
reMud <161937535+re-mud@users.noreply.github.com>
2025-04-04 18:21:09 +0500

    feat: add CommandService

---
+++ src/Application/Services/CommandService/CommandService.cs
@@ -0,0 +1,43 @@
+﻿using OmniVoice.Application.Command.CommandRecognition;
+using OmniVoice.Application.Services.SpeechRecognition;
+
+namespace OmniVoice.Application.Services.CommandService;
+
+public class CommandService
+{
+    private CommandRecognition _commandRecognition;
+    private SpeechRecognitionService _speechRecognitionService;
+
+    public CommandService(
+        CommandRecognition commandRecognition,
+        SpeechRecognitionService speechRecognitionService)
+    {
+        _commandRecognition = commandRecognition;
+        _speechRecognitionService = speechRecognitionService;
+
+        _speechRecognitionService.RecognitionCompleted += SpeechRecognitionService_RecognitionCompleted;
+    }
+
+    private void SpeechRecognitionService_RecognitionCompleted(object? sender, SpeechRecognition.Events.RecognitionEventArgs e)
+    {
+        CommandRecognitionResult[] results = _commandRecognition.Recognize(e.Text);
+
+        if (results.Length == 0) return;
+
+        CommandRecognitionResult best = results[0];
+        foreach (var result in results)
+        {
+            if (result.Probability > best.Probability)
+            {
+                best = result;
+            }
+        }
+
+        best.Execute();
+    }
+
+    public void Start()
+    {
+        _speechRecognitionService.Start();
+    }
+}
