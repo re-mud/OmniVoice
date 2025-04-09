dd175902821e625f0b5b23330d420ef94c9529ab
reMud <161937535+re-mud@users.noreply.github.com>
2025-04-09 23:58:19 +0500

    feat: add ISettings

---
+++ src/Domain/Services/ISettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace OmniVoice.Domain.Services;
+
+public interface ISettings
+{
+    T? GetConfiguration<T>(string key) where T : ISerializable;
+    void SetConfiguration<T>(T configuration, string key) where T : ISerializable;
+    /// <exception cref="ArgumentNullException"></exception>
+    void LoadFromJson(string json);
+    string ToJson();
+}
