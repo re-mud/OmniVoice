570723d8d857574bb4553b247ecf877328f73a75
reMud <161937535+re-mud@users.noreply.github.com>
2025-04-13 02:50:22 +0500

    feat: add logger

---
+++ src/Domain/Services/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace OmniVoice.Domain.Services;
+
+public interface ILogger
+{
+    void Info(string message);
+    void Debug(string message);
+    void Warn(string message);
+    void Error(string message);
+    void Fatal(string message);
+}
---
+++ src/Infrastructure/Services/Logger.cs
@@ -0,0 +1,30 @@
+﻿using OmniVoice.Domain.Services;
+
+namespace OmniVoice.Infrastructure.Services;
+
+public class Logger : ILogger
+{
+    private readonly object _lockFile = new();
+    private readonly string _filePath;
+
+    public Logger(string filePath)
+    {
+        _filePath = filePath;
+
+        File.WriteAllText(filePath, string.Empty);
+    }
+
+    private void Log(string type, string message)
+    {
+        lock (_lockFile)
+        {
+            File.AppendAllText(_filePath, $"[{type}] [{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}\n");
+        }
+    }
+
+    public void Info(string message) => Log("Info", message);
+    public void Debug(string message) => Log("Debug", message);
+    public void Warn(string message) => Log("Warn", message);
+    public void Error(string message) => Log("Error", message);
+    public void Fatal(string message) => Log("Fatal", message);
+}
