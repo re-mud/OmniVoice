3a0abc70b3d2bb23ec80b2f16afe59b2a4abca63
reMud <killersan0010@gmail.com>
2025-04-21 15:55:14 +0500

    feat(ServiceRegistration): add services

--- src/Infrastructure/Configuration/ServiceRegistration.cs
+++ src/Infrastructure/Configuration/ServiceRegistration.cs
@@ -7,6 +7,9 @@ using OmniVoice.Domain.SpeechRecognition.Interfaces;
 using OmniVoice.Infrastructure.Services;
 using OmniVoice.Infrastructure.Services.Logging;
 using OmniVoice.Infrastructure.Services.Logging.Options;
+using NAudio.Wave;
+
+using OmniVoice.Infrastructure.Services.Options;
 
 namespace OmniVoice.Infrastructure.Configuration;
 
@@ -14,13 +17,33 @@ public static class ServiceRegistration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<LoggerOptions>(configuration.GetSection(nameof(LoggerOptions)));
+        services.Configure<LoggerOptions>(configuration.GetSection("Logger"));
+        services.Configure<VoskModelOptions>(configuration.GetSection("VoskModel"));
+        services.Configure<VoskSpeechRecognitionOptions>(configuration.GetSection("VoskSpeechRecognition"));
+        services.Configure<WaveFormatOptions>(configuration.GetSection("WaveFormat"));
 
         services.AddSingleton<ILogger, Logger>();
 
-        services.AddTransient<IMicrophone, NAudioMicrophone>();
+        services.AddTransient(sp => CreateVoskModel(sp, configuration));
+        services.AddTransient<IMicrophone, NAudioMicrophone>(sp => CreateMicrophone(sp, configuration));
         services.AddTransient<ISpeechRecognition, VoskSpeechRecognition>();
 
         return services;
+    }
+
+    private static Vosk.Model CreateVoskModel(IServiceProvider sp, IConfiguration configuration)
+    {
+        var options = configuration.GetRequiredSection("WaveInEvent").Get<VoskModelOptions>();
+
+        return new Vosk.Model(options.ModelPath);
+    }
+
+    private static NAudioMicrophone CreateMicrophone(IServiceProvider sp, IConfiguration configuration)
+    {
+        var options = configuration.GetSection("WaveInEvent").Get<WaveFormatOptions>() ?? new WaveFormatOptions();
+        var waveIn = new WaveInEvent();
+        waveIn.WaveFormat = new WaveFormat(options.SampleRate, options.Bits, options.Channels);
+
+        return new NAudioMicrophone(waveIn);
     }
 }
