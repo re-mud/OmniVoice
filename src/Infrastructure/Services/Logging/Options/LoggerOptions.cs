90b5934e04d86a4bba2aa2c1d658190a447fa312
reMud <killersan0010@gmail.com>
2025-04-21 15:20:34 +0500

    feat(LoggerOptions): remove set

--- src/Infrastructure/Services/Logging/Options/LoggerOptions.cs
+++ src/Infrastructure/Services/Logging/Options/LoggerOptions.cs
@@ -2,12 +2,12 @@
 
 public class LoggerOptions
 {
-    public string LogPath { get; set; } = "logs.txt";
+    public string LogPath { get; } = "logs.txt";
     /// <summary>
     /// {0} - date
     /// {1} - log level
     /// {2} - message
     /// </summary>
-    public string LogFormat { get; set; } = "[{0}] [{1}] {2}\n";
-    public string DateFormat { get; set; } = "yyyy-MM-dd HH:mm:ss";
+    public string LogFormat { get; } = "[{0}] [{1}] {2}\n";
+    public string DateFormat { get; } = "yyyy-MM-dd HH:mm:ss";
 }
