403ee722a0793fbfc6cca8ad071f95d104402a21
reMud <killersan0010@gmail.com>
2025-04-21 14:23:27 +0500

    feat: add WaveFormatOptions

---
+++ src/Infrastructure/Services/Options/WaveFormatOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace OmniVoice.Infrastructure.Services.Options;
+
+public class WaveFormatOptions
+{
+    public int Bits { get; } = 16;
+    public int Channels { get; } = 1;
+    public int SampleRate { get; } = 16000;
+}
