cf9c7b9714efaaed1b0162bc315d5fa38712e7ab
reMud <161937535+re-mud@users.noreply.github.com>
2025-04-24 02:14:58 +0500

    fix(ServiceRegistration): register options & typo

--- src/Infrastructure/Configuration/ServiceRegistration.cs
+++ src/Infrastructure/Configuration/ServiceRegistration.cs
@@ -10,6 +10,7 @@ using OmniVoice.Infrastructure.Services.Logging.Options;
 using NAudio.Wave;
 
 using OmniVoice.Infrastructure.Services.Options;
+using Microsoft.Extensions.Options;
 
 namespace OmniVoice.Infrastructure.Configuration;
 
@@ -17,11 +18,19 @@ public static class ServiceRegistration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        // Configure options
         services.Configure<LoggerOptions>(configuration.GetSection("Logger"));
         services.Configure<VoskModelOptions>(configuration.GetSection("VoskModel"));
         services.Configure<VoskSpeechRecognitionOptions>(configuration.GetSection("VoskSpeechRecognition"));
         services.Configure<WaveFormatOptions>(configuration.GetSection("WaveFormat"));
 
+        // Register options
+        services.AddSingleton(sp => sp.GetRequiredService<IOptions<LoggerOptions>>().Value);
+        services.AddSingleton(sp => sp.GetRequiredService<IOptions<VoskModelOptions>>().Value);
+        services.AddSingleton(sp => sp.GetRequiredService<IOptions<VoskSpeechRecognitionOptions>>().Value);
+        services.AddSingleton(sp => sp.GetRequiredService<IOptions<WaveFormatOptions>>().Value);
+
+        // Register services
         services.AddSingleton<ILogger, Logger>();
 
         services.AddTransient(sp => CreateVoskModel(sp, configuration));
@@ -33,14 +42,14 @@ public static class ServiceRegistration
 
     private static Vosk.Model CreateVoskModel(IServiceProvider sp, IConfiguration configuration)
     {
-        var options = configuration.GetRequiredSection("WaveInEvent").Get<VoskModelOptions>();
+        var options = configuration.GetRequiredSection("VoskModel").Get<VoskModelOptions>();
 
         return new Vosk.Model(options.ModelPath);
     }
 
     private static NAudioMicrophone CreateMicrophone(IServiceProvider sp, IConfiguration configuration)
     {
-        var options = configuration.GetSection("WaveInEvent").Get<WaveFormatOptions>() ?? new WaveFormatOptions();
+        var options = sp.GetRequiredService<IOptions<WaveFormatOptions>>().Value;
         var waveIn = new WaveInEvent();
         waveIn.WaveFormat = new WaveFormat(options.SampleRate, options.Bits, options.Channels);
 
