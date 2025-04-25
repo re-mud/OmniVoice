0009c536364db49134dfd2ac5f012afe9c24f946
reMud <161937535+re-mud@users.noreply.github.com>
2025-04-25 20:04:21 +0500

    feat: add IdentifiedCommand

---
+++ src/Application/Models/IdentifiedCommand.cs
@@ -0,0 +1,16 @@
+﻿using OmniVoice.Domain.Command.Interfaces;
+using OmniVoice.Domain.Models;
+
+namespace OmniVoice.Application.Models;
+
+public class IdentifiedCommand : IIdentifiedEntity<ICommand>
+{
+    public string Id { get; set; }
+    public ICommand Value { get; set; }
+
+    public IdentifiedCommand(string id, ICommand value)
+    {
+        Id = id;
+        Value = value;
+    }
+}
