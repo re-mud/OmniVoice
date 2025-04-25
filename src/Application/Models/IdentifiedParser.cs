c4c82311a290097b403c57cfb3996cc034e8eb23
reMud <161937535+re-mud@users.noreply.github.com>
2025-04-25 20:09:06 +0500

    feat: add IdentifiedParser

---
+++ src/Application/Models/IdentifiedParser.cs
@@ -0,0 +1,16 @@
+﻿using OmniVoice.Domain.Analyzers;
+using OmniVoice.Domain.Models;
+
+namespace OmniVoice.Application.Models;
+
+public class IdentifiedParser : IIdentifiedEntity<IParser>
+{
+    public string Id { get; set; }
+    public IParser Value { get; set; }
+
+    public IdentifiedParser(string id, IParser value)
+    {
+        Id = id;
+        Value = value;
+    }
+}
