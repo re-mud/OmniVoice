14bcf4ba12cf9c55e046cf707ae0795851ce62ac
reMud <killersan0010@gmail.com>
2025-05-02 18:46:09 +0500

    feat: add RelayCommand

---
+++ src/Presentation/Common/Command/RelayCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace OmniVoice.Presentation.Common.Command;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action _execute;
+    private readonly Func<bool> _canExecute;
+
+    public RelayCommand(Action execute, Func<bool> canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
+
+    public void Execute(object parameter) => _execute();
+}
