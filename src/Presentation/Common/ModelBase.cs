4a243454c886b9bffe7bd8d8fbc19ed0f883f5b2
reMud <161937535+re-mud@users.noreply.github.com>
2025-05-15 00:24:44 +0500

    feat: add ModelBase

---
+++ src/Presentation/Common/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OmniVoice.Presentation.Common;
+
+public class ModelBase
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    }
+}
