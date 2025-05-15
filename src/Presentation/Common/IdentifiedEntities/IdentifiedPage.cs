d15a7e205655d8ff953a5c73a1143151546fe1c1
reMud <161937535+re-mud@users.noreply.github.com>
2025-05-15 22:35:10 +0500

    feat: add IdentifiedPage

---
+++ src/Presentation/Common/IdentifiedEntities/IdentifiedPage.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using OmniVoice.Domain.Command;
+using OmniVoice.Domain.Models;
+
+namespace OmniVoice.Application.Common.IdentifiedEntities;
+
+public class IdentifiedPage : IIdentifiedEntity<Page>
+{
+    public string Id { get; set; }
+    public Page Value { get; set; }
+
+    public IdentifiedPage(string id, Page value)
+    {
+        Id = id;
+        Value = value;
+    }
+}
