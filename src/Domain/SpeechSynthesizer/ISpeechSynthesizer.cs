62c0878d9716cc55841b479043d3abbc687a7a60
reMud <161937535+re-mud@users.noreply.github.com>
2025-05-22 22:49:42 +0500

    feat: add ISpeechSynthesizer

---
+++ src/Domain/SpeechSynthesizer/ISpeechSynthesizer.cs
@@ -0,0 +1,12 @@
+﻿namespace OmniVoice.Domain.SpeechSynthesizer;
+
+public interface ISpeechSynthesizer
+{
+    void Speak(string text);
+    void SpeakAsync(string text);
+    /// <param name="rate">[0.0f, 1.0f]</param>
+    void SetRate(float rate);
+    /// <param name="volume">[0.0f, 1.0f]</param>
+    void SetVolume(float volume);
+    void SelectVoice(string name);
+}
